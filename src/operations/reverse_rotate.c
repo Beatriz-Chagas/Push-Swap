1c758a682a3a369f7c42c16b1ad4f0bf84ac2738
Beatriz-Chagas <bchagas440@gmail.com>
2025-12-18 00:32:22 -0300

    inserindo as funções reverse_rotate

---
+++ src/operations/reverse_rotate.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchagas- <bchagas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 05:05:25 by bchagas-          #+#    #+#             */
+/*   Updated: 2025/12/18 00:29:17 by bchagas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rra(t_stack **a)
+{
+	ft_reverse_rotate(a);
+	ft_putendl_fd("rra", 1);
+}
+
+void	rrb(t_stack **b)
+{
+	ft_reverse_rotate(b);
+	ft_putendl_fd("rrb", 1);
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	ft_reverse_rotate(a);
+	ft_reverse_rotate(b);
+	ft_putendl_fd("rrr", 1);
+}
