defbc396fe75db0cea9f36a123717db21c806cae
Beatriz-Chagas <bchagas440@gmail.com>
2025-12-22 06:29:27 -0300

    corrigindo

--- src/operations/push.c
+++ src/operations/push.c
@@ -6,7 +6,7 @@
 /*   By: bchagas- <bchagas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 05:05:02 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/12/16 04:54:17 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/12/22 03:26:52 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	ft_pa(t_stack *a, t_stack *b)
 {
-    ft_push(a, b);
+    ft_push(b, a);
     ft_putendl_fd("pa", 1);
 }
 void	ft_pb(t_stack *a, t_stack *b)
 {
-    ft_push(b, a);
+    ft_push(a, b);
     ft_putendl_fd("pb", 1);
 }
