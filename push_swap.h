2373a1dd549a4ad6264312812046e9b715688102
Beatriz-Chagas <bchagas440@gmail.com>
2025-12-22 06:17:41 -0300

    acrescentando funções

--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,17 @@
 /*   By: bchagas- <bchagas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 04:21:18 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/12/16 04:56:38 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/12/22 04:48:20 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "../libft/libft.h"
+# include "libft/libft.h"
+
+# define INT_MAX 2147483647
+# define INT_MIN (-2147483647 - 1)
 
 typedef struct s_node
 {
@@ -39,17 +42,23 @@ typedef struct s_cost
 
 /* parsing */
 int		ft_parse(int argc, char **argv, t_stack *a);
+int		ft_has_duplicate(t_stack *a, int value);
+int		ft_push_number(t_stack *a, char *s);
+char	**ft_join_args(int argc, char **argv);
+int		ft_atol_safe(char *s, long *out);
 
 /* stack */
 t_stack	*ft_stacknew(void);
-void	ft_stack_free(t_stack *stack);
 void	ft_stack_push_back(t_stack *stack, int value);
+void	ft_stack_free(t_stack *stack);
 
 /* sort */
 void	ft_sort(t_stack *a, t_stack *b);
 void	ft_sort_small(t_stack *a);
+void	ft_sort_three(t_stack *a);
+void	ft_sort_five(t_stack *a, t_stack *b);
 
-/* Turk (simplificado) */
+/* Turk */
 void	ft_push_all_b(t_stack *a, t_stack *b);
 t_cost	ft_calculate_cost(t_stack *a, t_stack *b, t_node *node);
 t_node	*ft_find_cheapest(t_stack *a, t_stack *b);
@@ -59,12 +68,13 @@ void	ft_execute_cost(t_stack *a, t_stack *b, t_cost cost);
 t_node	*ft_find_min(t_stack *a);
 int		ft_get_pos(t_stack *a, t_node *n);
 int		ft_find_insert_pos(t_stack *a, int value);
+void	ft_bring_min_top(t_stack *a);
 
 /* operations */
-void	ft_swap(t_stack *s);
+void	ft_swap(t_stack *stack);
 void	ft_push(t_stack *from, t_stack *to);
-void	ft_rotate(t_stack *s);
-void	ft_reverse_rotate(t_stack *s);
+void	ft_rotate(t_stack *stack);
+void	ft_reverse_rotate(t_stack *stack);
 
 void	ft_sa(t_stack *a);
 void	ft_sb(t_stack *b);
@@ -80,4 +90,5 @@ void	ft_rr(t_stack *a, t_stack *b);
 void	ft_rra(t_stack *a);
 void	ft_rrb(t_stack *b);
 void	ft_rrr(t_stack *a, t_stack *b);
+
 #endif
