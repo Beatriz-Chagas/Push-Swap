61fc307a54cc28dced12f0359d2d99e55603b780
Beatriz-Chagas <bchagas440@gmail.com>
2025-12-25 06:52:13 -0300

    Fazendo testes com a main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bchagas- <bchagas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 01:39:24 by bchagas-          #+#    #+#             */
-/*   Updated: 2025/12/22 05:29:49 by bchagas-         ###   ########.fr       */
+/*   Updated: 2025/12/25 06:04:16 by bchagas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,31 +38,7 @@ int	ft_is_sorted(t_stack *a)
 // 	}
 // 	ft_printf("\n");
 // }
-#include <stdio.h>
 
-// int	main(int argc, char **argv)
-// {
-// 	t_stack	*a;
-// 	t_stack	*b;
-
-// 	if (argc < 2)
-// 		return (0);
-// 	a = ft_stacknew();
-// 	b = ft_stacknew();
-// 	if (!a || !b)
-// 		return (1);
-// 	if (ft_parse(argc, argv, a))
-// 		return (ft_putendl_fd("Error", 2), 1);
-// 	if (a->size <= 3)
-// 		ft_sort_small(a);
-// 	else if (a->size <= 5)
-// 		ft_sort_five(a, b);
-// 	//else
-// 	//t_sort(a, b);
-// 	ft_stack_free(a);
-// 	ft_stack_free(b);
-// 	return (0);
-// }
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
@@ -82,10 +58,11 @@ int	main(int argc, char **argv)
 			ft_sort_small(a);
 		else if (a->size <= 5)
 			ft_sort_five(a, b);
-//		else
-//			ft_sort(a, b);
+		else
+			ft_sort(a, b);
 	}
 	ft_stack_free(a);
 	ft_stack_free(b);
 	return (0);
 }
+
